1e6ad47bf1f894f2435420175c1af03ebdd222e2
Tesshu Hosoe <krokun600112@gmail.com>
2025-01-29 16:46:29 +0900

    シリアル通信テスト

--- sensor_py/sensor.py
+++ sensor_py/sensor.py
@@ -1,15 +1,20 @@
 import RPi.GPIO as GPIO
 import time
 import sys
+import serial
 
 trig_pin = 15
 echo_pin = 14
 speed_of_sound = 34370
 
+ser = serial.Serial('/dev/ttyACM0', 9600)
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(trig_pin, GPIO.OUT)
 GPIO.setup(echo_pin, GPIO.IN)
+
+time.sleep(2)
 
 print('now running...')
 
@@ -32,6 +37,8 @@ while True:
 	try:
 		distance = '{:.1f}'.format(get_distance())
 		print("Distance: " + distance + "cm")
+		send_str = f"{distance:.2f}\n"
+		ser.write(send_str.encode('utf-8'))
 		time.sleep(1)
 		
 	except KeyboardInterrupt:
